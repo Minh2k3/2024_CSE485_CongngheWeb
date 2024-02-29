a92f4421833f5e5e1133546e779bf109148149f9
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2024-03-01 01:24:37 +0700

    add project23

---
+++ project23/data.php
@@ -0,0 +1,9 @@
+<?php
+$users = [
+    [
+    "username" => "doquanghuy",
+    "password" => password_hash("huy123", PASSWORD_DEFAULT), 
+    "name" => "Quang Huy",
+    "email" => "huy@gmail.com"
+    ],
+   ];
---
+++ project23/profile.php
@@ -0,0 +1,23 @@
+<?php
+include 'data.php';
+
+session_start();
+if (!isset($_SESSION['user_id']) || !isset($_COOKIE['logged_in'])) {
+ header('Location: login.php'); // Redirect to login if not authenticated
+}
+$username = $_SESSION['user_id'];
+$user = null;
+foreach ($users as $u) {
+ if ($u['username'] === $username) {
+ $user = $u;
+ break;
+ }
+}
+if ($user) {
+ echo "Welcome, " . $user['name'] . "!";
+ echo "<br>Email: " . $user['email'];
+ echo '<br>'.'<a href="./logout.php">Log out</a>';
+} else {
+ echo "Error: User not found.";
+}
+?>
